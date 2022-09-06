0277852ccf70b718b5cbb30efbb39f6a8830386b
Rodrigo Devincenzi <rody_009@hotmail.com>
2022-09-06 15:31:30 -0300

    fix fondo

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Random;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.Point;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Color;
+
 
 public class Snake extends JFrame {
 
@@ -17,7 +20,9 @@ public class Snake extends JFrame {
     int heightPoint = 10;
     ImagenSnake imagenSnake;
     int direccion;
-    long frecuencia = 40;
+    long frecuencia = 30;
+    ArrayList<Point> lista;
+    boolean gameOver = false;
 
     public Snake() {
         setTitle("Snake");
@@ -25,6 +30,9 @@ public class Snake extends JFrame {
         Dimension ventana = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(ventana.width / 2 - width / 2, ventana.height / 2 - height / 2);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame.setDefaultLookAndFeelDecorated(false);
+        setUndecorated(true);
+
         Teclas teclas = new Teclas();
         this.addKeyListener(teclas);
         imagenSnake = new ImagenSnake();
@@ -38,34 +46,76 @@ public class Snake extends JFrame {
 
     public static void main(String[] args) throws Exception {
         Snake s = new Snake();
+
     }
 
-    public void startGame(){
-        comida = new Point(200,200);
-        snake = new Point(width / 2, height / 2);
+    public void startGame() {
+        comida = new Point(200, 100);
+        snake = new Point(320, 240);
+        lista = new ArrayList<Point>();
+        lista.add(snake);
         crearComida();
     }
 
-    public void crearComida(){
+    public void crearComida() {
         Random random = new Random();
-        comida.x = random.nextInt(width);
-        comida.y = random.nextInt(height);
+        
+        
+        comida.x = random.nextInt(630);
+
+        
+
+        comida.y = random.nextInt(470);
+
+        
     }
 
-    public void actualizar(){
-        imagenSnake.repaint();
-        if((snake.x > comida.x-10) && (snake.x > comida.x+10) && (snake.y > comida.y-10) && (snake.y > comida.y+10)){
-crearComida();
+    public void actualizar() {
+        
+        lista.add(0, new Point(snake.x, snake.y));
+        lista.remove(lista.size() - 1);
+
+        for (int i = 1; i < lista.size(); i++) {
+            Point punto = lista.get(i);
+            if (snake.x == punto.x && snake.y == punto.y) {
+                gameOver = true;
+            }
         }
+
+        if ((snake.x > (comida.x - 10)) && (snake.x < (comida.x + 10)) && (snake.y > (comida.y - 10))
+                && (snake.y < (comida.y + 10))) {
+            lista.add(0, new Point(snake.x, snake.y));
+            crearComida();
+        }
+
+        imagenSnake.repaint();
     }
 
     public class ImagenSnake extends JPanel {
+
+        
+
+        
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.setColor(new Color(0, 0, 255));
-            g.fillRect(snake.x, snake.y, widthPoint, heightPoint);
+            
+            if(gameOver){
+                g.setColor(new Color(0,0,0));
+            } else {
+                g.setColor(new Color(255,255,255));
+            }
 
-            g.setColor(new Color(255,0,0));
+            g.fillRect(0, 0, width, height);
+            g.setColor(new Color(0, 0, 255));
+
+            if(lista.size() > 0){
+                for (int i = 0; i < lista.size(); i++) {
+                    Point p = (Point) lista.get(i);
+                    g.fillRect(p.x, p.y, widthPoint, heightPoint);
+                }
+            }
+
+            g.setColor(new Color(255, 0, 0));
             g.fillRect(comida.x, comida.y, widthPoint, heightPoint);
 
         }
@@ -110,40 +160,42 @@ crearComida();
         public void run() {
             while (true) {
                 if (java.lang.System.currentTimeMillis() - last > frecuencia) {
-                    if (direccion == KeyEvent.VK_UP) {
-                        snake.y = snake.y - heightPoint;
-                        if(snake.y < 0){
-                            snake.y = height - heightPoint;
+                    if (!gameOver) {
+                        if (direccion == KeyEvent.VK_UP) {
+                            snake.y = snake.y - heightPoint;
+                            if (snake.y < 0) {
+                                snake.y = height - heightPoint;
+                            }
+                            if (snake.y > height) {
+                                snake.y = 0;
+                            }
                         }
-                        if(snake.y > height){
-                            snake.y = 0;
+                        if (direccion == KeyEvent.VK_DOWN) {
+                            snake.y = snake.y + heightPoint;
+                            if (snake.y < 0) {
+                                snake.y = height - heightPoint;
+                            }
+                            if (snake.y > height) {
+                                snake.y = 0;
+                            }
                         }
-                    }
-                    if (direccion == KeyEvent.VK_DOWN) {
-                        snake.y = snake.y + heightPoint;
-                        if(snake.y < 0){
-                            snake.y = height - heightPoint;
+                        if (direccion == KeyEvent.VK_LEFT) {
+                            snake.x = snake.x - widthPoint;
+                            if (snake.x < 0) {
+                                snake.x = width - widthPoint;
+                            }
+                            if (snake.x > width) {
+                                snake.x = 0;
+                            }
                         }
-                        if(snake.y > height){
-                            snake.y = 0;
-                        }
-                    }
-                    if (direccion == KeyEvent.VK_LEFT) {
-                        snake.x = snake.x - widthPoint;
-                        if(snake.x < 0){
-                            snake.x = width - widthPoint;
-                        }
-                        if(snake.x > width){
-                            snake.x = 0;
-                        }
-                    }
-                    if (direccion == KeyEvent.VK_RIGHT) {
-                        snake.x = snake.x + widthPoint;
-                        if(snake.x < 0){
-                            snake.x = width - widthPoint;
-                        }
-                        if(snake.x > width){
-                            snake.x = 0;
+                        if (direccion == KeyEvent.VK_RIGHT) {
+                            snake.x = snake.x + widthPoint;
+                            if (snake.x < 0) {
+                                snake.x = width - widthPoint;
+                            }
+                            if (snake.x > width) {
+                                snake.x = 0;
+                            }
                         }
                     }
 
